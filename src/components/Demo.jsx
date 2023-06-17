fd6549a81784737ccddabd6b7e27fc6d39cfc1d5
mazyaryousefinia <mazyaryousefinia@gmail.com>
2023-06-17 11:27:28 +0330

    Add display result

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -69,6 +69,7 @@ const Demo = () => {
           </button>
         </form>
 
+        {/* Browse History */}
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.map((item, index) => (
             <div
@@ -89,6 +90,34 @@ const Demo = () => {
             </div>
           ))}
         </div>
+      </div>
+
+      {/* Display Result */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
